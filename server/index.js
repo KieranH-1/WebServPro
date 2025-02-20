fda30671dae4220b5d14e8249d00a51030a73db5
KieranH-1 <heatonk1@newpaltz.edu>
2025-02-20 11:08:15 -0500

    class 2//17

---
+++ server/index.js
@@ -0,0 +1,18 @@
+/*
+*/
+// Load the http module to create an http server.
+const http = require('http');
+const PORT = 8000;
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+
+// Listen on port 8000, IP defaults to
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+console.log('Hello World!');
