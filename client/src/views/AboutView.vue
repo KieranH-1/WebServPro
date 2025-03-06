c1394af3b7140d69ab2490d150988bcd254f1c66
KieranH-1 <heatonk1@newpaltz.edu>
2025-03-06 14:34:41 -0500

    using vue use

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,10 +1,18 @@
+<script setup lang="ts">
+import { useNow } from '@vueuse/core'
+
+const { now, pause, resume } = useNow({ controls: true })
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <h2>About the best class at New Paltz</h2>
   </div>
+  <div>
+    <p>Current Time: {{ now }}</p>
+    <button class="button" @click="pause">Pause</button>
+    <button class="button" @click="resume">Resume</button>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
