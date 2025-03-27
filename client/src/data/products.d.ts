2058fca2c88953078eba1f69773721eeef87223f
KieranH-1 <heatonk1@newpaltz.edu>
2025-03-27 14:05:45 -0400

    new class

---
+++ client/src/data/products.d.ts
@@ -0,0 +1,52 @@
+interface Product {
+  id: number
+  title: string
+  description: string
+  category: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  tags: string[]
+  brand?: string
+  sku: string
+  weight: number
+  dimensions: {
+    width: number
+    height: number
+    depth: number
+  }
+  warrantyInformation: string
+  shippingInformation: string
+  availabilityStatus: string
+  reviews: Review[]
+  returnPolicy: string
+  minimumOrderQuantity: number
+  meta: Meta
+  images: string[]
+  thumbnail: string
+}
+
+interface Review {
+  rating: number
+  comment: string
+  date: string
+  reviewerName: string
+  reviewerEmail: string
+}
+
+interface Meta {
+  createdAt: string
+  updatedAt: string
+  barcode: string
+  qrCode: string
+}
+
+interface ProductsData {
+  products: Product[]
+  total: number
+  skip: number
+  limit: number
+}
+
+export { Product, Review, Meta, ProductsData }
--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-/*
-*/
+/*  B"H
+ */
 // Load the http module to create an http server.
-const express = require('express');
+const express = require("express");
+const productsController = require("./controllers/products");
+
 const PORT = 8000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello New Paltz, NY!!!')
-});
+app
+  .get("/", (req, res) => {
+    res.send("Hello New Paltz, NY!!!");
+  })
+  .use("/products", productsController);
 
 // Listen on port 8000, IP defaults to
+//
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`)
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
 
-console.log('Hello World!');
+console.log("Hello World!");
